b7e29b0e6417b8fde3d8c10b4c92216f8f57443c
Leonie Meiners <leoniemeiners@Leonies-MacBook-Pro.local>
2020-10-23 15:18:24 +0200

    changed the way method B works; no two-way swap anymore

--- sort_samples.py
+++ sort_samples.py
@@ -5,88 +5,110 @@ import re
 import pandas as pd
 
 
-def print_move(old_index, new_index, swapping):
-    # print how to swap or sort samples to achieve the correct order
+def print_move(old_index, new_index):
+    # print how to sort samples to achieve the correct order
+    print(f'{old_index+1} --> {new_index+1}')
 
-    if swapping:
-        arrow = '<-->'
+
+def print_moves(sorted_samples, inplace):
+
+    if inplace:
+       
+        check_sorting = [None] * len(sorted_samples) # check in the end if the sorting was done correctly
+        mapping = {sample[3]: new_index for new_index, sample in enumerate(
+            sorted_samples)}
+
+        # process the first sample
+        old_index = next(iter(mapping))  # get the first key of mapping
+        new_index = mapping[old_index]
+        print_move(old_index, new_index)
+        check_sorting[new_index] = old_index
+        del mapping[old_index]
+
+        while mapping:
+
+            try:
+                old_index = new_index
+                new_index = mapping[old_index]
+            # when filling in an already empty position:
+            except KeyError:  
+                old_index = next(iter(mapping))  # get the next key of mapping
+                new_index = mapping[old_index]
+
+            if old_index!=new_index:
+                print_move(old_index, new_index)
+
+            check_sorting[new_index] = old_index
+            del mapping[old_index]
+
+        assert([sample[3] for sample in sorted_samples] == check_sorting), 'sorting gone wrong'
+
+
     else:
-        arrow = '-->'
-
-    print(f'{old_index+1} {arrow} {new_index+1}')
-
-
-def print_moves(sample_tuples, sorted_sample_tuples, swapping):
-    # print how to sort samples into the new rack
-    for new_index, ele in enumerate(sorted_sample_tuples):
-        # there might be a problem if there is one sample twice ??, therefore, look at the last index where the element occurs,
-        # so that an already sorted sample is not moved again
-        old_index = len(sample_tuples) - 1 - sample_tuples[::-1].index(ele)
-
-        if swapping:
-            if old_index != new_index:
-                sample_tuples[old_index], sample_tuples[new_index] = sample_tuples[new_index], sample_tuples[old_index]
-                print_move(old_index, new_index, args.swapping)
-        else:
-            # put a None value in there in case of duplicates (so that a sample at a certain position isn't said to be moved
-            # twice)
-            sample_tuples[old_index] = None
-            print_move(old_index, new_index, args.swapping)
+        for new_index, sample in enumerate(sorted_samples):
+            # sample[3] is the old index
+            print_move(sample[3], new_index)
 
 
-def sample_type_sort(sample_type):
+def sample_type_order(sample_type):
 
     type_order = ('SE', 'AB', 'SP')
     return type_order.index(sample_type)
 
 
-def sample_sort(sample_tuples, swapping):
+def sample_sort(samples, inplace):
 
-    return sorted(sample_tuples, key=lambda sample_tuple: (sample_type_sort( 
-    	          sample_tuple[0]), sample_tuple[1], sample_tuple[2]))
+    return sorted(samples, key=lambda sample: (sample_type_order(
+                  sample[0]), sample[1], sample[2]))
 
 
 def main(args):
 
-    sample_tuples = []
+    samples = []
 
-    ## for tsv files:
+    # for tsv files:
     # with open(args.infile, 'r') as infile:
 
-    # 	for line in infile:
+    #   for line in infile:
 
-    # 		external_number = line.split('\t')[2]
+    #       external_number = line.split('\t')[2]
 
     df = pd.read_excel(args.infile)
 
-    for index, row in df.iterrows():
+    index = 0 # index for each sample
+    for i, row in df.iterrows():
 
         external_number = row[2]
 
         try:
-            # sample_tuple consists of sample_type, patient_id, swab_number
-            sample_tuple = re.search(
-                '(SE|AB|SP)([0-9]{4})(A[A-Z])', external_number).groups()
+            # sample consists of sample_type, patient_id, swab_number
+            sample = list(re.search(
+                '(SE|AB|SP)([0-9]{4})(A[A-Z])', external_number).groups())
+            # add an index to each sample to be able to map the old to the new
+            # indices
+            sample.append(index)
+            index+=1
         # continue if no match has been found
         except AttributeError:
             continue
 
-        sample_tuples.append(sample_tuple)
+        samples.append(sample)
 
-    sorted_sample_tuples = sample_sort(sample_tuples, args.swapping)
+    sorted_samples = sample_sort(samples, args.inplace)
 
-    print_moves(sample_tuples, sorted_sample_tuples, args.swapping)
+    print_moves(sorted_samples, args.inplace)
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-        description='sort samples by 1) sample type, 2) patient ID, 3) swap number')
+        description='sort samples by 1) sample type, 2) patient ID, 3) swap'
+        'number')
 
     parser.add_argument('infile', metavar='input_file', type=str)
 
-    parser.add_argument('--swapping', default=False, action='store_true', help=				   'Show how to swap samples instead of sorting them into'
-    	                'a new rack')
+    parser.add_argument('--inplace', default=False, action='store_true',
+                        help='Show how to sort the samples in-place instead of' 'sorting them into a new rack')
 
     args = parser.parse_args()
 
